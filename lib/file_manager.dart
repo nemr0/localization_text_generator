e9dc20ce48835493eedf43cdc110613c869c29fc
Omar Elnemr <omar.elnemr@nagwa.com>
2024-08-04 15:51:58 +0300

    fix fileManager.

--- lib/file_manager.dart
+++ lib/file_manager.dart
@@ -68,7 +68,7 @@ class FileManger {
         bool isScreenFile = result.$1;
         String content = result.$2;
         if (isScreenFile) {
-          _textMatcher.matchAndExtractTexts(content, file.path);
+          _textMatcher?.matchAndExtractTexts(content, file.path);
           acceptedFiles.add(file);
         }
       }
